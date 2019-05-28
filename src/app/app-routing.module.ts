a446b84523a1c5e749bc108a0db29028619459b6
aditya13jani <44555223+aditya13jani@users.noreply.github.com>
2019-05-28 12:41:28 -0400

    did it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from '../app/heroes/heroes.component';
+import { MessagesComponent } from '../app/messages/messages.component';
+import { DashboardComponent } from '../app/dashboard/dashboard.component';
+import { HeroDetailComponent } from '../app/hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: DashboardComponent, pathMatch: 'full' },
+  { path: "heroes", component: HeroesComponent },
+  { path: "detail/:id", component : HeroDetailComponent },
+  { path: "message", component: MessagesComponent },
+  { path:"**", redirectTo : "home" },
+  { path:"", redirectTo : "home", pathMatch : "full" }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
